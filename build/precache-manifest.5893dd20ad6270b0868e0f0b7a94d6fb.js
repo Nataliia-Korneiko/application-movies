0c58b6a601d6a9eb188991d670dbaec6e4f089e4
Nataliia Korneiko <korneyko_ns@ukr.net>
2020-03-11 16:11:20 +0200

    third day done

---
+++ build/precache-manifest.5893dd20ad6270b0868e0f0b7a94d6fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b734fd90b2ffc216f0e7f49ccdd78f",
+    "url": "/application-movies/index.html"
+  },
+  {
+    "revision": "fe6b3c31c00465135936",
+    "url": "/application-movies/static/css/main.21d6df8b.chunk.css"
+  },
+  {
+    "revision": "ad44621407cd1d9d81d2",
+    "url": "/application-movies/static/js/2.d49db511.chunk.js"
+  },
+  {
+    "revision": "d3d9d688f87a681fd5adaf231b9dfd7f",
+    "url": "/application-movies/static/js/2.d49db511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6b3c31c00465135936",
+    "url": "/application-movies/static/js/main.7c9f8ebd.chunk.js"
+  },
+  {
+    "revision": "d1a0d8af3ae069b71962",
+    "url": "/application-movies/static/js/runtime-main.e9726582.js"
+  }
+]);
